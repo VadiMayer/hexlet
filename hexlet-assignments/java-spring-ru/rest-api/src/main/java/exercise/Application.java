b162211d897f633bd5b98868f4340e266a2ead65
VadiMayer <h4998998482@gmail.com>
2023-11-13 18:02:14 +0300

    rest-api.rart2

--- hexlet-assignments/java-spring-ru/rest-api/src/main/java/exercise/Application.java
+++ hexlet-assignments/java-spring-ru/rest-api/src/main/java/exercise/Application.java
@@ -3,6 +3,8 @@ package exercise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static exercise.Data.getPosts;
+
 @SpringBootApplication
 public class Application {
 
--- hexlet-assignments/java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ hexlet-assignments/java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,36 +1,37 @@
 package exercise.controller.users;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import exercise.model.Post;
-import exercise.Data;
+import static exercise.Data.*;
 
 // BEGIN
 @RestController
 @RequestMapping("/api")
 public class PostsController {
 
-    @GetMapping("/users/{id}/posts")
-    public ResponseEntity<List<Post>> getPosts(@PathVariable int id) {
-        List<Post> posts = Data.getPosts().stream()
+    private final String URI = "/users/{id}/posts";
+
+    @GetMapping(URI)
+    public ResponseEntity<List<Post>> getPostsForUser(@PathVariable int id) {
+        List<Post> posts = getPosts().stream()
                 .filter(e -> e.getUserId() == id)
                 .toList();
         return ResponseEntity.ok()
                 .body(posts);
     }
 
-    @PostMapping("/users/{id}/posts")
-    public ResponseEntity<Post> create(@PathVariable int id, @RequestBody Post post) {
-
+    @PostMapping(URI)
+    public ResponseEntity<Post> create(@PathVariable int id, @RequestBody Post post) throws URISyntaxException {
+        post.setUserId(id);
+        getPosts().add(post);
+        return ResponseEntity.created(new URI("/users/" + id + "/posts"))
+                .body(post);
     }
 }
 // END
