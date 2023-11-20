449bcb1a1b0119b3daf93ca53c90d8c7fd38649c
VadiMayer <h4998998482@gmail.com>
2023-11-20 18:28:12 +0300

    jpa-data-end

--- hexlet-assignments/java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
+++ hexlet-assignments/java-spring-ru/jpa-data/src/main/java/exercise/model/Person.java
@@ -1,13 +1,29 @@
 package exercise.model;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 // BEGIN
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table()
+public class Person {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column()
+    private Long id;
+    @Column()
+    private String firstName;
+    @Column()
+    private String lastName;
+}
 // END
