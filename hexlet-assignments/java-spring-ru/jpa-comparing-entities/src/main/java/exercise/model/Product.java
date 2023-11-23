f7b61fb60764025e9f865ea22058feeb4a8fa6e8
VadiMayer <h4998998482@gmail.com>
2023-11-23 18:03:06 +0300

    jpa-comparing-entities

---
+++ hexlet-assignments/java-spring-ru/jpa-comparing-entities/src/main/java/exercise/model/Product.java
@@ -0,0 +1,27 @@
+package exercise.model;
+
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+// BEGIN
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"title", "price"})
+public class Product {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column()
+    private Long id;
+    @Column()
+    private String title;
+    @Column()
+    private int price;
+}
+// END
