2988dda159dd7277e5fbc5eb6501e019f5b92f64
VadiMayer <h4998998482@gmail.com>
2024-01-23 18:12:31 +0300

    search-part2

--- hexlet-assignments/java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
+++ hexlet-assignments/java-spring-ru/search/src/main/java/exercise/specification/ProductSpecification.java
@@ -6,13 +6,27 @@ import org.springframework.stereotype.Component;
 import exercise.dto.ProductParamsDTO;
 import exercise.model.Product;
 
+import java.io.IOException;
+
 // BEGIN
 @Component
 public class ProductSpecification {
     public Specification<Product> build(ProductParamsDTO paramsDTO) {
-        return null;
+        return withCategoryId(paramsDTO.getCategoryId())
+                .and(withPriceGt(paramsDTO.getPriceGt()));
+    }
+
+    private Specification<Product> withCategoryId(Long categoryId) {
+        return ((root, query, criteriaBuilder) ->
+                categoryId == null ? criteriaBuilder.conjunction() : criteriaBuilder.equal(root.get("categoryId"), categoryId));
+    }
+
+    private Specification<Product> withPriceGt(Integer priceGt) {
+        return ((root, query, criteriaBuilder) ->
+                priceGt == null ? criteriaBuilder.conjunction() : criteriaBuilder.greaterThan(root.get("priceGt"), priceGt));
     }
 
     
 }
+
 // END
